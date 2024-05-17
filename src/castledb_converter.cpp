cd52e8160853c8c1ce4814b0732ed8e21fad5624
Gustavo Domingues <61640594+bigasdev@users.noreply.github.com>
2024-05-17 07:40:50 -0300

    Created the cache + extract from name

--- src/castledb_converter.cpp
+++ src/castledb_converter.cpp
@@ -1,11 +1,13 @@
 #include "castledb_converter.hpp"
 
-CastleDBConverter::CastleDBConverter(const std::string filename)
+CastleDBConverter::CastleDBConverter(const std::string filename, bool update)
 {
     std::ifstream f(filename);
     data = json::parse(f);
 
-    update_data();
+    m_has_update = update;
+
+    if(m_has_update) update_data();
 
     m_last_edited = std::to_string(std::filesystem::last_write_time(filename).time_since_epoch().count());
 }
@@ -48,46 +50,35 @@ const std::vector<CastleData> CastleDBConverter::extract_data(const std::string 
     return result;
 }
 
-const CastleData CastleDBConverter::extract_data_from_name(const std::string sheetName, const std::string name, std::vector<std::string> keys)
+//Extract data from the cache using the name
+CastleData CastleDBConverter::extract_data_from_name(const std::string name, std::vector<std::string> keys)
 {
-    for(auto& sheet : data["sheets"]){
-        if(sheet["name"] == sheetName){
-            for(auto& line : sheet["lines"]){
-                if(line["Name"] == name){
-                    CastleData castleData;
-                    castleData.name = line["Name"];
+    if(m_cache.find(name) != m_cache.end()){
+        CastleData castleData = m_cache[name];
+        CastleData result;
+        result.name = castleData.name;
 
-                    for(auto& key : keys){
-                        if(line[key].is_null())continue;
-
-                        if(line[key].is_number()){
-                            castleData.data[key] = std::to_string(line[key].get<int>());
-                            continue;
-                        }
-                        if(line[key].is_boolean()){
-                            castleData.data[key] = std::to_string(line[key].get<bool>());
-                            continue;
-                        }
-
-                        castleData.data[key] = line[key];
-                    }
-
-                    return castleData;
-                }
+        for(auto& key : keys){
+            if(castleData.data.find(key) != castleData.data.end()){
+                result.data[key] = castleData.data[key];
             }
         }
+
+        return result;
     }
 
     return CastleData();
 }
 
+//Update method will check the last write time of the file to parse the data again
+//It updates the cache if enabled too
 bool CastleDBConverter::update(const std::string filename)
 {
     if(m_last_edited != std::to_string(std::filesystem::last_write_time(filename).time_since_epoch().count())){
         std::ifstream f(filename);
         data = json::parse(f);
 
-        update_data();
+        if(m_has_update) update_data();
 
         m_last_edited = std::to_string(std::filesystem::last_write_time(filename).time_since_epoch().count());
         return true;
@@ -95,6 +86,7 @@ bool CastleDBConverter::update(const std::string filename)
     return false;
 }
 
+//Updates the cache data
 void CastleDBConverter::update_data()
 {
     for(auto& sheet : data["sheets"]){
--- src/castledb_converter.hpp
+++ src/castledb_converter.hpp
@@ -16,17 +16,19 @@ struct CastleData{
 
 class CastleDBConverter {
 public:
-    CastleDBConverter(const std::string filename);
+    CastleDBConverter(const std::string filename, bool update = false);
     ~CastleDBConverter();
 
     const std::vector<CastleData> extract_data(const std::string sheetName, std::vector<std::string> keys);
-    const CastleData extract_data_from_name(const std::string sheetName, const std::string name, std::vector<std::string> keys);
+    CastleData extract_data_from_name(const std::string name, std::vector<std::string> keys);
     bool update(const std::string filename);
 private:
     void update_data();
 
     json data;
     std::string m_last_edited;
+
+    bool m_has_update = false;
 
     std::unordered_map<std::string, CastleData> m_cache;
 }; 
